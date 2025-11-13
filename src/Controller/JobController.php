dc536ddf3a0fe02e0d1aada77537b1bd3871f389
meriemsakhri <meriem.sakhri@polytechnicien.tn>
2025-11-13 23:10:17 +0100

    feat: Complete ORM, Forms and CRUD implementation

--- src/Controller/JobController.php
+++ src/Controller/JobController.php
@@ -9,6 +9,12 @@ use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Job;
 use App\Entity\Image;
 use App\Entity\Candidature;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\HttpFoundation\Request;
+use App\Form\JobType;
 
 class JobController extends AbstractController
 {
@@ -77,6 +83,120 @@ class JobController extends AbstractController
         return $this->render('job/show.html.twig', [
             'job' => $job,
             'listCandidatures' => $listCandidatures,
+        ]);
+    }
+
+    #[Route(path: "/Ajouter", name: "add_candidat")]
+    public function ajouter_cand(Request $request, EntityManagerInterface $em): Response
+    {
+        $candidat = new Candidature();
+        $fb = $this->createFormBuilder($candidat)
+            ->add('candidat', TextType::class)
+            ->add('contenu', TextType::class, options: array("label" => "Content"))
+            ->add('date', DateType::class)
+            ->add('job', EntityType::class, options: [
+                'class' => Job::class,
+                'choice_label' => 'type',
+            ])
+            ->add('Valider', SubmitType::class);
+        $form = $fb->getForm();
+
+        // Injection dans la base de données
+        $form->handleRequest($request);
+        if ($form->isSubmitted()) {
+            $em->persist($candidat);
+            $em->flush();
+            
+            // Add flash message
+            $this->addFlash('notice', 'Candidat ajouté avec succès');
+            
+            return $this->redirectToRoute('home');
+        }
+
+        return $this->render('job/ajouter.html.twig', [
+            'f' => $form->createView(),
+            'is_editing' => false
+        ]);
+    }
+
+    #[Route(path: "/add", name: "add_job")]
+    public function ajouter2(Request $request, EntityManagerInterface $em): Response
+    {
+        $job = new Job();
+        $form = $this->createForm(JobType::class, $job);
+        $form->handleRequest($request);
+        if ($form->isSubmitted()) {
+            $em->persist($job);
+            $em->flush();
+            return $this->redirectToRoute('home');
+        }
+        return $this->render('job/ajouter.html.twig', [
+            'f' => $form->createView(),
+            'is_editing' => false
+        ]);
+    }
+
+    #[Route("/", name: "home")]
+    public function home(EntityManagerInterface $em): Response
+    {
+        $repo = $em->getRepository(Candidature::class);
+        $lesCandidats = $repo->findAll();
+        
+        return $this->render('job/home.html.twig',
+            ['lesCandidats' => $lesCandidats]);
+    }
+
+    #[Route(path: "/supp/{id}", name: "cand_delete")]
+    public function delete(Request $request, $id, EntityManagerInterface $entityManager): Response 
+    {
+        $c = $entityManager
+            ->getRepository(Candidature::class)
+            ->find($id);
+            
+        if (!$c) {
+            throw $this->createNotFoundException(
+                'No candidature found for id '.$id
+            );
+        }
+        
+        $entityManager->remove($c);
+        $entityManager->flush();
+        
+        return $this->redirectToRoute('home');
+    }
+
+    #[Route('/edit/{id}', name: 'edit_user', methods: ['GET', 'POST'])]
+    public function edit(Request $request, $id, EntityManagerInterface $em): Response
+    {
+        $candidat = $em->getRepository(Candidature::class)->find($id);
+        
+        if (!$candidat) {
+            throw $this->createNotFoundException(
+                'No candidat found for id '.$id
+            );
+        }
+        
+        $fb = $this->createFormBuilder($candidat)
+            ->add('candidat', TextType::class)
+            ->add('contenu', TextType::class, array("label" => "Content"))
+            ->add('date', DateType::class)
+            ->add('job', EntityType::class, [
+                'class' => Job::class,
+                'choice_label' => 'type',
+            ])
+            ->add('Valider', SubmitType::class);
+            
+        $form = $fb->getForm();
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted()) {
+            $em->flush();
+            return $this->redirectToRoute('home');
+        }
+        
+        return $this->render('job/ajouter.html.twig', [
+            'f' => $form->createView(),
+            'is_editing' => true
         ]);
     }
 }
---
+++ src/Form/JobType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Job;
+use App\Entity\Image;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class JobType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('type')
+            ->add('company')
+            ->add('description')
+            ->add('expires_at')
+            ->add('email')
+            ->add('image', EntityType::class, [
+                'class' => Image::class,
+                'choice_label' => 'id',
+            ])
+            ->add('Valider', SubmitType::class);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Job::class,
+        ]);
+    }
+}
